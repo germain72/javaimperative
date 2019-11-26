0adb1b84ebc2ed61e7a6adf1825b5aa2733156b2
germain72 <36983066+germain72@users.noreply.github.com>
2019-11-26 11:36:16 +0100

    Create TypeCasting.java
    CGE

---
+++ src/TypeCasting.java
@@ -0,0 +1,38 @@
+/**
+*
+*/
+public class TypeCasting {
+
+	public static void main(String[] args) {
+
+//		testImplicit();
+		testExplicit();
+	}
+	
+	public static void testImplicit() {
+
+		byte var1 = 127;
+		short var2 = 32_767;
+
+		System.out.println(var1);
+		System.out.println(var2);
+		
+		// Widening casting
+		var2 = var1;
+		System.out.println(var1);
+	}
+	
+	public static void testExplicit() {
+		
+		byte var1 = 127;
+		short var2 = 32_767;
+		
+		System.out.println(var1);
+		System.out.println(var2);
+
+		// Narrowing casting
+		var1 = (byte) var2;
+		System.out.println(var1);
+	}
+
+}
